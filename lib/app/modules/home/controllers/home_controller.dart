c1a33b22c73156b8d8e42b7b881694c9cf4213b6
Dwiiiiii <ilaldwisyah2016@gmail.com>
2023-03-13 20:29:35 +0700

    pdf upadte

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -33,15 +33,32 @@ class HomeController extends GetxController {
     final pdf = pw.Document();
 
     pdf.addPage(pw.Page(
-        pageFormat: PdfPageFormat.a4,
+        pageFormat: PdfPageFormat(30 * PdfPageFormat.mm, 58 * PdfPageFormat.mm,
+            marginAll: 2 * PdfPageFormat.mm),
         build: (pw.Context context) {
           return pw.Column(children: [
-            pw.Center(child: pw.Text('$tKode')),
-            pw.Center(child: pw.Text('$tSPBU')),
-            pw.Row(children: [pw.Text('$tShift'), pw.Text('$tTrans')]),
-            pw.Text('$tWaktu'),
+            pw.Center(
+                child: pw.Text(' ${tKode.text} ',
+                    style: pw.TextStyle(fontSize: 5))), //kode
             pw.SizedBox(height: 10),
-            pw.Row(children: [pw.Text('Pulau/Pompa :'), pw.Text('$tPompa')])
+            pw.Center(
+                child: pw.Text('${tSPBU.text} ',
+                    style: pw.TextStyle(fontSize: 5))), //spbu
+            pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Text('Shift: ${tShift.text}',
+                      style: pw.TextStyle(fontSize: 4)), //shift
+                  pw.Text('No. Trans: ${tTrans.text}',
+                      style: pw.TextStyle(fontSize: 4)) //no trans
+                ]),
+            pw.Text('Waktu: ${tWaktu.text}', style: pw.TextStyle(fontSize: 3)),
+            pw.SizedBox(height: 10),
+            ///////22222
+            pw.Text('Pulau/Pompa : ${tPompa.text}',
+                style: pw.TextStyle(fontSize: 4)),
+            pw.Text('Nama Produk : ${tProduk.text}',
+                style: pw.TextStyle(fontSize: 4)),
           ]);
         }));
 
